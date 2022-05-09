2f1d8c2a259f695452261356cf2efa245e4f5533
lenaStsk <staskevichalena@gmail.com>
2022-05-09 22:51:40 +0300

    feat: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   entry: './js/index.js',
-  mode: 'development',
+  mode: 'production',
   output: {
-    filename: 'bundle.js',
+    filename: 'dist/bundle.js',
     path: __dirname,
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'index.html', to: 'dist/' },
+        { from: 'style.css', to: 'dist/' },
+      ],
+    }),
+  ],
 };
